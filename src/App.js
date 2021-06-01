6a2bba47133730cf4ad1af66d61a290608a54ea1
XxAndreyX <andrey.gubanov.95@mail.ru>
2021-06-01 10:28:43 +0300

    hw6

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import './App.css';
-import React, { useState } from 'react';
-import ToDo from './components/ToDo';
+import Home from './projects/Home.js';
+import EditTodo from './projects/EditTodo.js';
 
-class MytodoList extends React.Component{
-  render(){
-    return(
-      <ToDo/>
-    )
+import TaskList from './components/TaskList/TaskList.js';
+import React, { useState } from 'react';
+// react-router-dom
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+const state = [ 
+  { id:1, 
+    name: "Завести новую задачу", 
+    description: 'Для выполнения всех задач на день', 
+    tasks: [
+      {
+        id: 4321,
+        name: '',
+        description: '',
+        completed: false
+      }
+    ],
+    completed: false
   }
-}
+];
 
 const App = () =>{
-  return(
-  <div className = 'App'>
-    <h1>MyList</h1>
-    <MytodoList/>
-  </div>)
-
+    return (
+      <Router>
+            <div style={{ padding: 8 }}>
+              <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/todos/:id" component={EditTodo} />
+              </Switch>
+            </div>
+      </Router>
+    )
 }
+
 export default App;
