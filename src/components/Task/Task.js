d67f9bb428367abf56a3a91f545769ff79b3dadd
XxAndreyX <andrey.gubanov.95@mail.ru>
2021-06-01 11:04:36 +0300

    hw6

---
+++ src/components/Task/Task.js
@@ -0,0 +1,33 @@
+import './Task.css';
+import Edit from '../../projects/EditTodo';
+import {Link} from 'react-router-dom'
+
+const Task = (props) => {    
+
+    const classes = ['task'];
+    if (props.todo.completed) {
+      classes.push('done')
+    }
+    return (
+      <div className={'task'}>
+        <span className={classes.join(' ')}> 
+          <input type='checkbox'onChange={()=> props.onChange(props.todo.id)}/>
+          <div className={'id'}>{props.index}</div>
+          <div>
+            <div>
+              <Link to={{
+                pathname: `/todos/${props.todo.id}`,
+                propsSearch: props,
+                createChildren: props.createChildren,
+                todos: props.todos,
+                parentId: props.todo.id
+              }}>{props.todo.name}</Link>
+            </div>
+            <div className={'description'}>{props.todo.description}</div>
+          </div>
+        </span>
+        <button className={'close-button'} onClick={() => props.onRemove(props.todo.id)}>&times; </button>
+      </div>
+    )
+};
+export default Task;
